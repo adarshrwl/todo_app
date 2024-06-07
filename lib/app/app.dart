719ad9e078a518093d7fca3cbf8742e37c152285
Adarsh Rawal <adarshrawal321@gmail.com>
2024-06-07 09:09:57 +0545

    more files

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/screen/home_screen.dart';
+import 'package:to_do_app/view/todo_app_view.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          // ···
+          brightness: Brightness.dark,
+        ),
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+          // ···
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      title: 'Riverpod Test',
+      home: const MyTodoApp(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:to_do_app/app/app.dart';
+import 'package:to_do_app/view%20model/todo_viewmodel.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TodoViewModel(),
+      child: const App(),
+    ),
+  );
 }
